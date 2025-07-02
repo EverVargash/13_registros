549996d1879e9281038f4f1f6492ce8b0130db49
EverVargash <ebvargash@unjbg.edu.pe>
2025-07-01 20:29:51 -0500

    Inicio de modularizacion

---
+++ Contacto/encabezadodcont.h
@@ -0,0 +1,8 @@
+#ifndef ENCABEZADODCONT
+#define ENCABEZADODCONT
+
+void leerCorreo(correo &, string, string);
+void leerContacto(contactoEmail &, string, char, int, correo);
+void imprimeContacto(contactoEmail &);
+
+#endif
